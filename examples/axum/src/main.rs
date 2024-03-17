6aeac24ac3570c65520ba16d646be03df5da3cbc
Cédric Meuter <cedric.meuter@gmail.com>
2024-03-17 16:17:20 +0100

    first-ish version of a generic REST router

--- examples/axum/src/main.rs
+++ examples/axum/src/main.rs
@@ -1,15 +1,99 @@
-use axum::{
-    extract::{Path, State},
-    http::StatusCode,
-    response::IntoResponse,
-    routing::{delete, get},
-    Json, Router,
-};
+use std::net::SocketAddr;
+
+mod router {
+    use axum::{
+        routing::{delete, get},
+        Router,
+    };
+    use miniorm::{traits::Schema, CrudStore};
+    use serde::Serialize;
+    use sqlx::{postgres::PgRow, FromRow};
+
+    pub fn create_rest_router<E, S>(store: CrudStore<E>) -> Router<S>
+    where
+        E: Schema + for<'r> FromRow<'r, PgRow> + Unpin + Send + Serialize + Clone + Sync + 'static,
+    {
+        Router::new()
+            .route("/", get(handler::list))
+            .route("/", delete(handler::delete_all))
+            .route("/:id", delete(handler::delete))
+            .route("/:id", get(handler::read))
+            .with_state(store)
+    }
+
+    mod handler {
+
+        use axum::{
+            extract::{Path, State},
+            http::StatusCode,
+            response::IntoResponse,
+            Json,
+        };
+        use miniorm::{traits::Schema, CrudStore};
+        use serde::Serialize;
+        use sqlx::{postgres::PgRow, FromRow};
+
+        pub(crate) async fn list<E>(
+            State(store): State<CrudStore<E>>,
+        ) -> Result<impl IntoResponse, StatusCode>
+        where
+            E: Schema + for<'r> FromRow<'r, PgRow> + Unpin + Send + Serialize,
+        {
+            store
+                .list()
+                .await
+                .map(Json)
+                .map_err(|_| StatusCode::INTERNAL_SERVER_ERROR)
+        }
+
+        pub(crate) async fn read<E>(
+            Path(id): Path<i64>,
+            State(todos): State<CrudStore<E>>,
+        ) -> Result<impl IntoResponse, StatusCode>
+        where
+            E: Schema + for<'r> FromRow<'r, PgRow> + Unpin + Send + Serialize,
+        {
+            todos
+                .read(id)
+                .await
+                .map(|e| Json(e.into_inner()))
+                .map_err(|_| StatusCode::INTERNAL_SERVER_ERROR)
+        }
+
+        pub(crate) async fn delete<E>(
+            Path(id): Path<i64>,
+            State(todos): State<CrudStore<E>>,
+        ) -> Result<impl IntoResponse, StatusCode>
+        where
+            E: Schema + for<'r> FromRow<'r, PgRow> + Unpin + Send + Serialize,
+        {
+            todos
+                .delete(id)
+                .await
+                .map(|_| Json(()))
+                .map_err(|_| StatusCode::INTERNAL_SERVER_ERROR)
+        }
+
+        pub(crate) async fn delete_all<E>(
+            State(todos): State<CrudStore<E>>,
+        ) -> Result<impl IntoResponse, StatusCode>
+        where
+            E: Schema + for<'r> FromRow<'r, PgRow> + Unpin + Send + Serialize,
+        {
+            todos
+                .delete_all()
+                .await
+                .map(|_| Json(()))
+                .map_err(|_| StatusCode::INTERNAL_SERVER_ERROR)
+        }
+    }
+}
+
+use axum::Router;
 use miniorm::{CrudStore, Schema};
 use serde::{Deserialize, Serialize};
 use sqlx::{FromRow, PgPool};
-
-type BoxResult<T> = std::result::Result<T, Box<dyn std::error::Error>>;
+use tokio::net::TcpListener;
 
 #[derive(Debug, Clone, Eq, PartialEq, FromRow, Schema, Serialize, Deserialize)]
 struct Todo {
@@ -28,89 +112,30 @@ impl Todo {
     }
 }
 
-type TodoStore = CrudStore<Todo>;
-
 #[tokio::main]
-async fn main() -> BoxResult<()> {
+async fn main() -> Result<(), Box<dyn std::error::Error>> {
     // connect to postgres
     dotenv::dotenv().ok();
     let url = std::env::var("DATABASE_URL").expect("missing DATABASE_URL env");
     let db = PgPool::connect(&url).await?;
 
     // initialize todo store
-    let todos = TodoStore::new(db);
+    let todos = CrudStore::new(db);
     todos.recreate_table().await?;
     todos.create(Todo::new("do the laundry")).await?;
     todos.create(Todo::new("wash the dishes")).await?;
     todos.create(Todo::new("go walk the dog")).await?;
     todos.create(Todo::new("groceries")).await?;
 
-    // create and start the app
-    let app = Router::new()
-        .route("/todos/:id/done", get(mark_as_done))
-        .route("/todos/:id", delete(delete_todo))
-        .route("/todos/:id", get(read_todo))
-        .route("/todos", get(list_todos))
-        .with_state(todos);
-    let listener = tokio::net::TcpListener::bind("127.0.0.1:3000")
-        .await
-        .unwrap();
+    let app = Router::new().nest("/todos", router::create_rest_router(todos));
+
+    let addr = SocketAddr::from(([127, 0, 0, 1], 3000));
     println!("listening...");
-    println!("- try: http://{}/todos", listener.local_addr().unwrap());
-    println!("- try: http://{}/todos/2", listener.local_addr().unwrap());
-    println!(
-        "- try: http://{}/todos/3/done",
-        listener.local_addr().unwrap()
-    );
+    println!("- try: http://{}/todos", addr);
+    println!("- try: http://{}/todos/2", addr);
+
+    let listener = TcpListener::bind(&addr).await.unwrap();
     axum::serve(listener, app).await?;
 
     Ok(())
-}
-
-async fn list_todos(State(todos): State<TodoStore>) -> Result<impl IntoResponse, StatusCode> {
-    todos
-        .list()
-        .await
-        .map(Json)
-        .map_err(|_| StatusCode::INTERNAL_SERVER_ERROR)
-}
-
-async fn read_todo(
-    Path(id): Path<i64>,
-    State(todos): State<TodoStore>,
-) -> Result<impl IntoResponse, StatusCode> {
-    todos
-        .read(id)
-        .await
-        .map(|e| Json(e.into_inner()))
-        .map_err(|_| StatusCode::INTERNAL_SERVER_ERROR)
-}
-
-async fn delete_todo(
-    Path(id): Path<i64>,
-    State(todos): State<TodoStore>,
-) -> Result<impl IntoResponse, StatusCode> {
-    todos
-        .delete(id)
-        .await
-        .map(|_| Json(()))
-        .map_err(|_| StatusCode::INTERNAL_SERVER_ERROR)
-}
-
-async fn mark_as_done(
-    Path(id): Path<i64>,
-    State(todos): State<TodoStore>,
-) -> Result<impl IntoResponse, StatusCode> {
-    let mut todo = todos
-        .read(id)
-        .await
-        .map_err(|_| StatusCode::INTERNAL_SERVER_ERROR)?;
-
-    todo.inner.done = true;
-
-    todos
-        .update(todo)
-        .await
-        .map(|e| Json(e.into_inner()))
-        .map_err(|_| StatusCode::INTERNAL_SERVER_ERROR)
 }
