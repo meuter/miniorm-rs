ddae696e7e7095db59f03593243b6826232777a0
Cédric Meuter <cedric.meuter@gmail.com>
2024-04-01 15:30:05 +0200

    Bind -> BindColumn

--- src/traits/mod.rs
+++ src/traits/mod.rs
@@ -1,9 +1,9 @@
-mod bind;
+mod bind_col;
 mod crud;
 mod schema;
 mod sqlx_ext;
 
-pub use bind::Bind;
+pub use bind_col::BindColumn;
 pub use crud::{Create, Crud, Delete, Update};
 pub use schema::Schema;
 pub use sqlx_ext::{BindableQuery, RowsAffected, SupportsReturning};
