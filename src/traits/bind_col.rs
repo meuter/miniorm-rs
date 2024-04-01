f2bfca6345d92bc051c3b7c2529ba1b7648b254b
Cédric Meuter <cedric.meuter@gmail.com>
2024-04-01 15:31:22 +0200

    BindableQuery -> Bind

--- src/traits/bind_col.rs
+++ src/traits/bind_col.rs
@@ -1,4 +1,4 @@
-use super::sqlx_ext::BindableQuery;
+use super::sqlx_ext::Bind;
 use sqlx::database::Database;
 
 /// Trait that can be implemented on a `struct` to bind the fields of
@@ -7,7 +7,7 @@ use sqlx::database::Database;
 /// # Example
 ///
 /// ```
-/// use miniorm::{BindColumn, BindableQuery};
+/// use miniorm::{BindColumn, Bind};
 /// use sqlx::Postgres;
 ///
 /// struct Todo {
@@ -18,7 +18,7 @@ use sqlx::database::Database;
 /// impl BindColumn<Postgres> for Todo {
 ///     fn bind_column<'q, Q>(&self, query: Q, column_name: &'static str) -> Q
 ///     where
-///         Q: ::miniorm::BindableQuery<'q, Postgres> {
+///         Q: ::miniorm::Bind<'q, Postgres> {
 ///         match column_name {
 ///             "description" => query.bind(self.description.clone()),
 ///             "done" => query.bind(self.done.clone()),
@@ -36,5 +36,5 @@ pub trait BindColumn<DB: Database> {
     /// binds a specific column using the provided query.
     fn bind_column<'q, Q>(&self, query: Q, column_name: &'static str) -> Q
     where
-        Q: BindableQuery<'q, DB>;
+        Q: Bind<'q, DB>;
 }
--- src/traits/mod.rs
+++ src/traits/mod.rs
@@ -6,4 +6,4 @@ mod sqlx_ext;
 pub use bind_col::BindColumn;
 pub use crud::{Create, Crud, Delete, Update};
 pub use schema::Schema;
-pub use sqlx_ext::{BindableQuery, RowsAffected, SupportsReturning};
+pub use sqlx_ext::{Bind, RowsAffected, SupportsReturning};
